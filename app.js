2a53607b5db109b304558110fde3b1c2b2253ecc
Asma-Abed <abed.asma@hotmail.com>
2022-11-07 18:59:47 +0100

    populating reviews

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-app.use('/api/v1/review', reviewRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
