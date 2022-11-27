5eb47ddf3d1dfd06ccf6ded8f3c5751bdb69a7d5
Asma-Abed <abed.asma@hotmail.com>
2022-11-27 11:50:06 +0100

    setting pug engine with express

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -15,7 +16,14 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // Global middlewares
+
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // set security http headers
 app.use(helmet());
 
@@ -55,9 +63,6 @@ app.use(
     ],
   })
 );
-
-// serving static files
-app.use(express.static(`${__dirname}/public`));
 
 // test middleware
 app.use((req, res, next) => {
