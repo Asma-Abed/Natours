3fe8e0a8423f3b1ea3b4173ee5fe0b4c402e752f
Asma-Abed <abed.asma@hotmail.com>
2022-11-27 17:22:53 +0100

    adding card to overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1. get tours data from collection
+  const tours = await Tour.find();
+  // 2. build our template
+
+  // 3. render the template
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
