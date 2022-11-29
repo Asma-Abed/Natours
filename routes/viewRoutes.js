a84dd4856c416709f11f6d7c062128dc6c4bcdad
Asma-Abed <abed.asma@hotmail.com>
2022-11-29 18:25:26 +0100

    rendering tour page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get('/', viewsController.getOverview);
 
-router.get('/tour', viewsController.getTour);
+router.get('/tour/:slug', viewsController.getTour);
+
+// router.get('/tours/:name', );
 
 module.exports = router;
