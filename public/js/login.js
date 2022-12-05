e9c39c5436a0c4692c257a4eb81404277e21317b
Asma-Abed <abed.asma@hotmail.com>
2022-12-05 19:19:01 +0100

    refactoring after using parcel

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
 /*eslint-disable*/
-
-const login = async (email, password) => {
+import axios from 'axios';
+export const login = async (email, password) => {
+  console.log('Login');
   try {
     const res = await axios({
       method: 'POST',
@@ -21,10 +22,3 @@ const login = async (email, password) => {
     alert(err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
