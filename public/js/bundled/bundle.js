85920cf9cd70af6c1a10f72f67f5161bed7798c0
Asma-Abed <abed.asma@hotmail.com>
2022-12-08 18:27:31 +0100

    account page is built

--- public/js/bundled/bundle.js
+++ public/js/bundled/bundle.js
@@ -21936,7 +21936,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62483" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61941" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
