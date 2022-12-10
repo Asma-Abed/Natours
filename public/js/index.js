bf8b2dc126ecebaada157ebf2887647e748e5610
Asma-Abed <abed.asma@hotmail.com>
2022-12-10 09:47:17 +0100

    updating user data using our API

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,11 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 // import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 if (loginForm)
   loginForm.addEventListener('submit', (e) => {
@@ -16,3 +18,11 @@ if (loginForm)
   });
 
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
