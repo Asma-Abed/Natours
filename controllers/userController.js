a374c8cce38be95cd3d4e107a220e17891298748
Asma-Abed <abed.asma@hotmail.com>
2022-12-18 13:54:25 +0100

    processing multiple images

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,16 +33,16 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
   if (!req.file) return next();
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
   next();
-};
+});
 
 const filterObj = (obj, ...allowedFirelds) => {
   const newObj = {};
