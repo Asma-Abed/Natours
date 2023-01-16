17d89941f32147563afff6bdc12c8e3ef4591653
Asma-Abed <abed.asma@hotmail.com>
2023-01-16 19:54:30 +0100

    adding stripe to the frontend with a js script

---
+++ public/js/stripe.js
@@ -0,0 +1,6 @@
+/*eslint-disable*/
+const stripe = Stripe(
+  'pk_test_51MQAGsLXTkp0ne7OKLxANCx8Ksn7kSEOajM4NOjQ2aXyJcnTu9ZVU1CktuEobGu27SxmgZmQH23wcAsjdxuHsCin00HaatzMDP'
+);
+
+export const bookTour = (tourId) => {};
