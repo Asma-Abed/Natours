9e5aaf6e7427520aaceff5ade609a8c583bda603
Asma-Abed <abed.asma@hotmail.com>
2023-01-20 13:37:41 +0100

    creating new bookings implemented

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const bookingSchema = mongoose.Schema({
   tour: {
-    type: mongoose.Schema.objectId,
+    type: mongoose.Schema.ObjectId,
     ref: 'Tour',
     required: [true, 'Booking must belong to a Tour!'],
   },
   user: {
-    type: mongoose.Schema.objectId,
+    type: mongoose.Schema.ObjectId,
     ref: 'User',
     required: [true, 'Booking must belong to a User!'],
   },
