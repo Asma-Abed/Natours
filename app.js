326ed23808ae40d17ba4ad178da4c26b2ef74cf2
Asma-Abed <abed.asma@hotmail.com>
2023-01-22 11:43:27 +0100

    CORS implemented

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,15 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Global middlewares
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// if we have api.natours.com, but frontend is natours.com, we need to allow for our frontent to access api
+//app.use(cors({
+// origin: 'https://www.natours.com
+// }))
+
+app.options('*', cors());
 
 // serving static files
 app.use(express.static(path.join(__dirname, 'public')));
